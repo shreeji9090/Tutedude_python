75a5090cd58ac69362f53db75b7a04ae6e096908
shreeji@9090 <111887157+shreeji9090@users.noreply.github.com>
2025-06-06 14:20:02 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+user1 = int(input("enter number 1"))
+user2 = int(input("enter number 2"))
+
+print("addition is ",user1 + user2)
+print("substraction is ",user1 - user2)
+print("division is ",user1 / user2)
+print("multiplication is ",user1 * user2)
