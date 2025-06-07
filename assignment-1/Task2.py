06fb99c27b6d6a1b39280003a8c5434e937768e1
shreeji@9090 <111887157+shreeji9090@users.noreply.github.com>
2025-06-07 15:10:57 +0530

    Add files via upload

---
+++ assignment-1/Task2.py
@@ -0,0 +1,7 @@
+greeting1 =("hello, ")
+greeting2 =(" welcome to the python program")
+
+firstname =input("enter your first name")
+lastname = input("enter your last name")
+
+print(greeting1 + firstname + lastname + greeting2)
