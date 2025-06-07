b2c63f7d406b8839413e1c271e49b83838e20186
shreeji@9090 <111887157+shreeji9090@users.noreply.github.com>
2025-06-07 15:05:08 +0530

    Delete Task1.py

--- Task1.py
+++
@@ -1,7 +0,0 @@
-user1 = int(input("enter number 1"))
-user2 = int(input("enter number 2"))
-
-print("addition is ",user1 + user2)
-print("substraction is ",user1 - user2)
-print("division is ",user1 / user2)
-print("multiplication is ",user1 * user2)
